477c38479dd5a75b19e9b54b755830db0b50d456
Laura Vonessen <laurav4@cs.washington.edu>
2018-02-18 02:00:36 -0800

    Filtered courses

--- src/coursechoice.js
+++ src/coursechoice.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import _ from 'lodash';
 
 import { Topic } from './topic';
 import { Unknown } from './unknown';
@@ -20,19 +21,20 @@ class CourseChoice extends React.Component {
 
 		var appCourses = _.map(_.filter(courses, { 'area': "applications" }), (course) => { return (<li key={course.id} role="presentation"><CourseOption course={course.id} name={course.name} update={this.props.update} selected={this.props.isSelected}></CourseOption></li>); });
 
-		var courseoptions = [];
-		for (var i = 0; i<courses.length; i++){
-			var course = courses[i];
-			// pass course *INDEX* into update to toggle selection
-			courseoptions.push(<li key={course.id} role="presentation"><CourseOption course={course.id} name={course.name} update={this.props.update} selected={this.props.isSelected}></CourseOption></li>);
+		var areas = ["applications", "prog_sys", "systems","theory","ai"];
+		var prettyAreas = ["Applications", "Programming Systems", "Systems","Theory","AI"];
+
+		var rows = [];
+		var courseSubset = [];
+		for (var i=0; i<areas.length; i++){
+			courseSubset[i] = _.map(_.filter(courses, { 'area': areas[i] }), (course) => { return (<li key={course.id} role="presentation"><CourseOption course={course.id} name={course.name} update={this.props.update} selected={this.props.isSelected}></CourseOption></li>); });
+			rows.push(<h2 key={areas[i]+"title"}>{prettyAreas[i]}</h2>);
+			rows.push(<ul key={areas[i]+"content"}>{courseSubset[i]}</ul>);
 		}
 
 		return (
 			<div className="container">
-				<ul>
-				{courseoptions}
-				{appCourses}
-				</ul>
+				{rows}
 
 			</div>
 		)
