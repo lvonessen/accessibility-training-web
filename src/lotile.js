4a96cd36eea805570932c92793aab8ce9cd0a2cf
Laura Vonessen <laurav4@cs.washington.edu>
2018-02-19 08:01:22 -0800

    Preliminary html content for one topic.

--- src/lotile.js
+++ src/lotile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { NestedList } from './nestedlist';
+import Interweave from 'interweave';
 
 class LOTile extends React.Component {
 
@@ -23,43 +24,41 @@ class LOTile extends React.Component {
 	render() {
 
 		// set-up things:
-		//console.log(this.props.material)
-
-		var material = (<NestedList list={this.props.material} id={this.props.id+"_material"} key={this.props.id+"_material"}/>)
-
-
-		//console.log(this.props.options)
+		var material;
+		// the first option takes a nested array of strings and makes a nested bulleted listToHtml
+		// the second takes a string of arbitrary html and performs some sanitations on it, then displays it
+		if (this.props.material.constructor === Array) {
+			material = (<NestedList list={this.props.material} id={this.props.id+"_material"} key={this.props.id+"_material"}/>)
+		} else {
+			material = (<Interweave tagName="div" content={""+this.props.material}/>)
+		}
 
 		var options = "";
 
 		if (this.props.options!= undefined){
 			options = (<NestedList list={this.props.options} id={this.props.id+"_options"} key={this.props.id+"_options"}/>)
 		}
-		//this.listToHtml(this.props.options,[]);
 
 		var blah = "<ul> <li> Hi </li> </ul>";
 
-		// return things:
-		{/* [
-	{
-		"id": "lo_what",
-		"name": "Accessibility is about designing to take into account all kinds of abilities",
-		"arg": "This is important because...",
-		"material": [ "point 1", "point 2", ["subpoint1", "subpoint2"], "point 3" ],
-		"options": [ "brief", "indepth", "activity" ]
-	}
-]  */}
+		// structure of subtopic values:
+		/*
+			{
+				"id": "lo_what",
+				"name": "Accessibility is about designing to take into account all kinds of abilities",
+				"arg": "This is important because...",
+				"material": [ "point 1", "point 2", ["subpoint1", "subpoint2"], "point 3" ],
+				"options": [ "brief", "indepth", "activity" ]
+			}
+		*/
 
 		return (
 			<div name={ this.props.name } className="lotile ws-bottom">
 
-				<h3>{this.props.name }</h3>
+				<h3><Interweave tagName="span" content={this.props.name}/></h3>
+				{/*<h3>{this.props.name }</h3>*/}
 				<p> {this.props.arg } </p>
 				<div>{material}</div>
-				<div>
-				<h4> How do you want to teach this topic? </h4>
-				{options}
-				</div>
 			</div>
 		)
 	}
